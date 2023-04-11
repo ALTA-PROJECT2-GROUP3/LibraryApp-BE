77b8dfade738229d399c11075723c418efe9e5fe
unknown <adiyudapranata@gmail.com>
2023-04-11 09:29:56 +0700

    create user entity

--- features/users/UsersEntity.go
+++ features/users/UsersEntity.go
@@ -1,1 +1,22 @@
 package users
+
+import "mime/multipart"
+
+type Core struct {
+	Id       uint
+	Name     string `validate:"required"`
+	Username string `validate:"required"`
+	Email    string `validate:"required,email"`
+	Address  string `validate:"required"`
+	Phone    string `validate:"required"`
+	Password string `validate:"required,min=3"`
+	Pictures string
+}
+
+type UserService interface {
+	Register(newUser Core, file *multipart.FileHeader) error
+}
+
+type UserData interface {
+	Register(newUser Core) error
+}
