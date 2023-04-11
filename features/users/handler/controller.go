a7d16201e38d7fa7aadf98e493ac400b382e60ca
unknown <adiyudapranata@gmail.com>
2023-04-11 09:49:32 +0700

    create user handler for register

--- features/users/handler/controller.go
+++ features/users/handler/controller.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"libraryapp/features/users"
+	"libraryapp/helper"
+	"net/http"
+
+	"github.com/jinzhu/copier"
+	"github.com/labstack/echo/v4"
+)
+
+type UserHandler struct {
+	srv users.UserService
+}
+
+func New(service users.UserService) *UserHandler {
+	return &UserHandler{
+		srv: service,
+	}
+}
+
+func (uh *UserHandler) Register(c echo.Context) error {
+	registerInput := RegisterRequest{}
+	if err := c.Bind(&registerInput); err != nil {
+		return c.JSON(helper.ErrorResponse(err))
+	}
+	newUser := users.Core{}
+	copier.Copy(&newUser, &registerInput)
+	err := uh.srv.Register(newUser)
+	if err != nil {
+		return c.JSON(helper.ErrorResponse(err))
+	}
+	return c.JSON(helper.SuccessResponse(http.StatusCreated, "register successfully"))
+}
