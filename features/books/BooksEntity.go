89e0d3d3c1694b9300a1c78d3b0554a61b6941b6
unknown <adiyudapranata@gmail.com>
2023-04-12 01:34:54 +0700

    Create books entity

--- features/books/BooksEntity.go
+++ features/books/BooksEntity.go
@@ -1,1 +1,12 @@
 package books
+
+type Core struct {
+	Id          uint
+	Title       string `validate:"required"`
+	Publisher   string `validate:"required"`
+	Year        string `validate:"required"`
+	Description string `validate:"required"`
+	Pictures    string
+	UserID      uint
+	UserName    string
+}
