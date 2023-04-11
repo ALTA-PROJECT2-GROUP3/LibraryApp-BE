43b4a867749529eb09317b8f2785ea4984d99641
unknown <adiyudapranata@gmail.com>
2023-04-11 09:28:22 +0700

    create router user register

--- app/router/router.go
+++ app/router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	_userData "libraryapp/features/users/data"
+	_userHandler "libraryapp/features/users/handler"
+	_userService "libraryapp/features/users/services"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitRouter(db *gorm.DB, e *echo.Echo) {
+	userData := _userData.New(db)
+	userSrv := _userService.New(userData)
+	userHdl := _userHandler.New(userSrv)
+	e.POST("/register", userHdl.Register)
+}
