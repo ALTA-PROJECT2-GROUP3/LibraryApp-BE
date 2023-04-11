f8d6f8aeb7d858e57154d823c794b309b8bb28af
unknown <adiyudapranata@gmail.com>
2023-04-11 09:53:21 +0700

    create user data for register

--- features/users/data/model.go
+++ features/users/data/model.go
@@ -1,1 +1,44 @@
 package data
+
+import (
+	"libraryapp/features/users"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Username string `gorm:"unique"`
+	Email    string `gorm:"unique"`
+	Password string
+	Address  string
+	Phone    string
+	Pictures string
+}
+
+func CoreToUser(data users.Core) User {
+	return User{
+		Model:    gorm.Model{ID: data.Id},
+		Name:     data.Name,
+		Username: data.Username,
+		Email:    data.Email,
+		Password: data.Password,
+		Address:  data.Address,
+		Phone:    data.Phone,
+		Pictures: data.Pictures,
+	}
+}
+
+func UserToCore(data User) users.Core {
+	return users.Core{
+		Id:       data.ID,
+		Name:     data.Name,
+		Username: data.Username,
+		Email:    data.Email,
+		Password: data.Password,
+		Address:  data.Address,
+		Phone:    data.Phone,
+		Pictures: data.Pictures,
+	}
+}
--- features/users/data/query.go
+++ features/users/data/query.go
@@ -1,1 +1,27 @@
 package data
+
+import (
+	"libraryapp/features/users"
+
+	"gorm.io/gorm"
+)
+
+type userQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) users.UserData {
+	return &userQuery{
+		db: db,
+	}
+}
+
+// Register implements users.UserData
+func (uq *userQuery) Register(newUser users.Core) error {
+	data := CoreToUser(newUser)
+	tx := uq.db.Create(&data)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
