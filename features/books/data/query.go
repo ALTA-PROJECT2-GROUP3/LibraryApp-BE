bc9d09b1b26fda070f8b692defbc373cacbc702e
unknown <adiyudapranata@gmail.com>
2023-04-12 02:14:27 +0700

    Create add book query

--- features/books/data/query.go
+++ features/books/data/query.go
@@ -1,1 +1,27 @@
 package data
+
+import (
+	"libraryapp/features/books"
+
+	"gorm.io/gorm"
+)
+
+type bookQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) books.BookData {
+	return &bookQuery{
+		db: db,
+	}
+}
+
+// Insert implements books.BookData
+func (bk *bookQuery) Insert(input books.Core) error {
+	data := CoreToBook(input)
+	tx := bk.db.Create(&data)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
