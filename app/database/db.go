db22ad0c0dfa3160ca1188d6b25d5c286b983590
unknown <adiyudapranata@gmail.com>
2023-04-11 09:20:25 +0700

    Build database

--- app/database/db.go
+++ app/database/db.go
@@ -1,1 +1,35 @@
 package database
+
+import (
+	"libraryapp/app/config"
+	// users "libraryapp/features/users/data"
+	// books "libraryapp/features/books/data"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDBMySql(cfg config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DB_USER,
+		cfg.DB_PASS,
+		cfg.DB_HOST,
+		cfg.DB_PORT,
+		cfg.DB_NAME,
+	)
+
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		log.Println("error connect to database:", err.Error())
+		return nil
+	}
+	return db
+}
+
+// func InitialMigration(db *gorm.DB) {
+// 	db.AutoMigrate(&users.User{})
+// 	db.AutoMigrate(&books.Book{})
+// }
