413e9889cbff83c88f0ca400ebd555f09d6fa255
unknown <adiyudapranata@gmail.com>
2023-04-11 16:17:13 +0700

    fix1 user register

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"libraryapp/app/config"
+	"libraryapp/app/database"
+	"libraryapp/app/router"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.InitConfig()
+	db := database.InitDBMySql(*cfg)
+	database.InitialMigration(db)
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	router.InitRouter(db, e)
+	e.Logger.Fatal(e.Start(":80"))
+}
