2ae0d3de6ec599f0b5c8512dcfc6c66221c597a4
unknown <adiyudapranata@gmail.com>
2023-04-12 03:00:47 +0700

    Create get all book data

--- features/books/data/query.go
+++ features/books/data/query.go
@@ -16,6 +16,18 @@ func New(db *gorm.DB) books.BookData {
 	}
 }
 
+// SelectAll implements books.BookData
+func (bk *bookQuery) SelectAll(limit int, offset int, name string) ([]books.Core, error) {
+	nameSearch := "%" + name + "%"
+	var roomsModel []Book
+	tx := bk.db.Limit(limit).Offset(offset).Where("rooms.name LIKE ?", nameSearch).Select("rooms.id, rooms.name, rooms.price, rooms.pictures, rooms.availability, rooms.description, rooms.location, users.name AS user_name, AVG(feedbacks.rating) AS rating_room").Joins("JOIN users ON rooms.user_id = users.id").Joins("LEFT JOIN feedbacks ON rooms.id = feedbacks.room_id").Group("rooms.id").Find(&roomsModel)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	roomsCoreAll := ListModelToCore(roomsModel)
+	return roomsCoreAll, nil
+}
+
 // Insert implements books.BookData
 func (bk *bookQuery) Insert(input books.Core) error {
 	data := CoreToBook(input)
