2c9c3c7ca99865172771d1580b2e2fce111f5e0b
unknown <adiyudapranata@gmail.com>
2023-04-12 23:46:49 +0700

    create rents select by id router

--- app/router/router.go
+++ app/router/router.go
@@ -34,12 +34,13 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	e.GET("/books", bookHdl.GetAll)
 	e.PUT("/books/:id", bookHdl.Update, middlewares.JWTMiddleware())
 	e.GET("/mybook", bookHdl.MyBook, middlewares.JWTMiddleware())
-	e.GET("/booksbyid/:id", bookHdl.GetBookById)
-	e.DELETE("/deletebooks/:id", bookHdl.DeleteBook, middlewares.JWTMiddleware())
+	e.GET("/books/:id", bookHdl.GetBookById)
+	e.DELETE("/books/:id", bookHdl.DeleteBook, middlewares.JWTMiddleware())
 
 	rentData := _rentData.New(db)
 	rentSrv := _rentService.New(rentData)
 	rentHdl := _rentHandler.New(rentSrv)
 	e.POST("/rents", rentHdl.Add, middlewares.JWTMiddleware())
+	e.GET("/rents/:id", rentHdl.GetById)
 
 }
