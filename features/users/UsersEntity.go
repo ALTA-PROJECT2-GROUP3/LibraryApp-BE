1e19ff60ab62d58500b55d9d81643e1ccc89b4bd
iqballazuardi <iqballazuardi12@gmail.com>
2023-04-12 10:37:36 +0700

    add getUserbyId

--- features/users/UsersEntity.go
+++ features/users/UsersEntity.go
@@ -15,10 +15,12 @@ type UserService interface {
 	Register(newUser Core) error
 	Login(username string, password string) (string, Core, error)
 	Update(id int, updateUser Core) error
+	UserByID(id int) (Core, error)
 }
 
 type UserData interface {
 	Register(newUser Core) error
 	Login(username string) (Core, error)
 	Update(id int, updateUser Core) error
+	UserByID(id int) (Core, error)
 }
