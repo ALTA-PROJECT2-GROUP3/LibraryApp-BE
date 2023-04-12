57ef5795209a4a5c097b73368a32cd8acb1c63a2
unknown <adiyudapranata@gmail.com>
2023-04-12 23:00:28 +0700

    create rents services

--- features/rents/services/services.go
+++ features/rents/services/services.go
@@ -17,3 +17,17 @@ func New(repo rents.RentData) rents.RentService {
 		vld:  validator.New(),
 	}
 }
+
+// Create implements rents.RentService
+func (srv *rentService) Create(newRent rents.Core) error {
+	errValidate := srv.vld.Struct(newRent)
+	if errValidate != nil {
+		return errValidate
+	}
+
+	errInsert := srv.data.Insert(newRent)
+	if errInsert != nil {
+		return errInsert
+	}
+	return nil
+}
