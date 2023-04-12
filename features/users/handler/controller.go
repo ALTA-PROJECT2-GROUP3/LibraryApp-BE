ae4f6b87e833d75699da554ed000ae82e05103f1
iqballazuardi <iqballazuardi12@gmail.com>
2023-04-12 10:34:36 +0700

    add controller getbyid

--- features/users/handler/controller.go
+++ features/users/handler/controller.go
@@ -5,6 +5,7 @@ import (
 	"libraryapp/helper"
 	"libraryapp/middlewares"
 	"net/http"
+	"strconv"
 
 	"github.com/jinzhu/copier"
 	"github.com/labstack/echo/v4"
@@ -37,10 +38,12 @@ func (uh *UserHandler) Register(c echo.Context) error {
 func (uh *UserHandler) Login(c echo.Context) error {
 	loginInput := LoginRequest{}
 	if err := c.Bind(&loginInput); err != nil {
+		c.Logger().Error("terjadi kesalahan bind", err.Error())
 		return c.JSON(helper.ErrorResponse(err))
 	}
 	token, data, err := uh.srv.Login(loginInput.Username, loginInput.Password)
 	if err != nil {
+		c.Logger().Error("terjadi kesalahan ")
 		return c.JSON(helper.ErrorResponse(err))
 	}
 	res := LoginResponse{}
@@ -62,4 +65,18 @@ func (uh *UserHandler) Update(c echo.Context) error {
 	}
 	return c.JSON(helper.SuccessResponse(http.StatusCreated, "UpdateProfile successfully"))
 
+}
+
+func (uh *UserHandler) UserByID(c echo.Context) error {
+	userID, errCnv := strconv.Atoi(c.Param("id"))
+	if errCnv != nil {
+		return errCnv
+	}
+	data, err := uh.srv.UserByID(userID)
+	if err != nil {
+		return c.JSON(helper.ErrorResponse(err))
+	}
+	res := users.Core{}
+	copier.Copy(&res, &data)
+	return c.JSON(helper.SuccessResponse(http.StatusOK, "profile successfully displayed", res))
 }
